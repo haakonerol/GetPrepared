b01f6bf3d10749a2365ecb5a6b37cdc84d652e0f
haakonerol <haakon.erol@gmail.com>
2025-01-30 14:52:26 +0100

    controller updated

--- ProductsAPI_ASP.NetCore6/ProductsAPI/Controllers/ProductController.cs
+++ ProductsAPI_ASP.NetCore6/ProductsAPI/Controllers/ProductController.cs
@@ -9,23 +9,24 @@ namespace ProductsAPI.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        private static List<ProductEntity> _products = new List<ProductEntity>
+        private readonly DataContext _context;
+
+        public ProductController(DataContext context)
         {
-            new ProductEntity { Id = 1, Name = "Apple", Price = "1000" },
-            new ProductEntity { Id = 2, Name = "Samsung", Price = "9000" },
-            new ProductEntity { Id = 3, Name = "Huawei", Price = "8000" }
-        };
+            _context = context;
+        }
+
 
         [HttpGet]
         public async Task<ActionResult<List<ProductEntity>>> Get()
         {
-            return Ok(_products);
+            return Ok(await _context.ProductEntities.ToListAsync());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<ProductEntity>> GetById(int id)
         {
-            var product = _products.Find(p => p.Id == id);
+            var product = await _context.ProductEntities.FindAsync(id);
             if(product == null)
                 return BadRequest("Product not found");
             return Ok(product);
@@ -34,29 +35,32 @@ namespace ProductsAPI.Controllers
         [HttpPost]
         public async Task<ActionResult<List<ProductEntity>>> AddProduct(ProductEntity product)
         {
-            _products.Add(product);
-            return Ok(_products);
+            _context.ProductEntities.Add(product);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.ProductEntities.ToListAsync());
         }
 
         [HttpPut("{id}")]
         public async Task<ActionResult<List<ProductEntity>>> UpdateProduct(int id, ProductEntity request)
         {
-            var product = _products.Find(p => p.Id == request.Id);
+            var product = await _context.ProductEntities.FindAsync(request.Id);
             if(product == null)
                 return BadRequest("Product not found to update");
             product.Name = request.Name;
             product.Price = request.Price;
-            return Ok(product);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.ProductEntities.ToListAsync());
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<List<ProductEntity>>> DeleteProduct(int id)
         {
-            var product = _products.Find(p => p.Id == id);
+            var product = await _context.ProductEntities.FindAsync(id);
             if(product == null)
                 return BadRequest("Product not found to delete");
-            _products.Remove(product);
-            return Ok(_products);
+            _context.ProductEntities.Remove(product);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.ProductEntities.ToListAsync());
         }
 
     }
