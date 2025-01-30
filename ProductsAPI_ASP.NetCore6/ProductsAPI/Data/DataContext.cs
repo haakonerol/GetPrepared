3b6b516e2f6495d6c4fbbb6c89e8cbd1ea5d4942
haakonerol <haakon.erol@gmail.com>
2025-01-30 14:25:52 +0100

    datacontext added

--- ProductsAPI_ASP.NetCore6/ProductsAPI/Data/DataContext.cs
+++ ProductsAPI_ASP.NetCore6/ProductsAPI/Data/DataContext.cs
@@ -1,8 +1,11 @@
-using Microsoft.EntityFrameworkCore;
+
+using ProductsAPI.Core;
 
 namespace ProductsAPI.Data;
 
 public class DataContext :DbContext
 {
     public DataContext(DbContextOptions<DataContext> options): base(options){ }
+
+    public DbSet<ProductEntity> ProductEntities { get; set; }
 }
