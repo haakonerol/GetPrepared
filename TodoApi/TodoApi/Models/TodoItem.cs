ab943a77e2232a67e574e96e6742a9b9e67bcbb7
haakonerol <haakon.erol@gmail.com>
2025-02-11 14:23:27 +0100

    controller added

--- TodoApi/TodoApi/Models/TodoItem.cs
+++ TodoApi/TodoApi/Models/TodoItem.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TodoApi.Models;
 
 public class TodoItem
 {
     public int Id { get; set; }
+    [MaxLength(30)]
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
 }
